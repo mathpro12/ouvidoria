9947744b27677c6ec53457c9217e754714a83c7c
Matheus Procópio <matheusproc@hotmail.com>
2017-11-11 00:16:00 -0200

    melhoras nas barras de navegacao

--- resources/views/layouts/partials/default-navigation.blade.php
+++ resources/views/layouts/partials/default-navigation.blade.php
@@ -1,15 +1,16 @@
 <div class="container">
-    <div class="col-md-7 col-md-offset-2">
+    <div class="col-md-10 col-md-offset-1">
         <nav class="navbar navbar-default">
             <div class="container-fluid">
 
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    </button>
                     <a class="navbar-brand" href="/">Prefeitura de Brumadinho</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li><a href="{{ route('get.follow-request') }}">Consultar Protocolo</a></li>
+                    </ul>
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="{{ route('get.register') }}">Cadastrar</a></li>
                         <li><a href="{{ route('login') }}">Login</a></li>
