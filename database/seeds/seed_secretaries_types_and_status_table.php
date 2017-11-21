942efc0d7dc2393df451a9d000163c27ddac5325
Matheus Procópio Lima <matheusproc@hotmail.com>
2017-11-21 11:41:39 -0200

    alteraSeed

--- database/seeds/seed_secretaries_types_and_status_table.php
+++ database/seeds/seed_secretaries_types_and_status_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,24 +14,24 @@ class seed_secretaries_types_and_status_table extends Seeder
     public function run()
     {
         DB::statement("
-            INSERT INTO types (name, description) VALUES ('Solicitação', 'Solitações Diversas');
-            INSERT INTO types (name, description) VALUES ('Reclamação', 'Reclamações Diversas');
-            INSERT INTO types (name, description) VALUES ('Sugestão', 'Sugestões Diversas');
-            INSERT INTO types (name, description) VALUES ('Denúncia', 'Denúncias Diversas');
-            INSERT INTO types (name, description) VALUES ('Elogio', 'Elogios Diversas');
+            INSERT INTO types (name, description, created_at, updated_at) VALUES ('Solicitação', 'Solitações Diversas', Carbon::now(), Carbon::now());
+            INSERT INTO types (name, description, created_at, updated_at) VALUES ('Reclamação', 'Reclamações Diversas', Carbon::now(), Carbon::now());
+            INSERT INTO types (name, description, created_at, updated_at) VALUES ('Sugestão', 'Sugestões Diversas', Carbon::now(), Carbon::now());
+            INSERT INTO types (name, description, created_at, updated_at) VALUES ('Denúncia', 'Denúncias Diversas', Carbon::now(), Carbon::now());
+            INSERT INTO types (name, description, created_at, updated_at) VALUES ('Elogio', 'Elogios Diversas', Carbon::now(), Carbon::now());
 
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Ação Social', 'Valcir Carlos Martins');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Administração', 'Flávio Capdeville de Meira');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Agricultura, Desenvolvimento Econômico, Pecuária e Abastecimento', 'Andressa Rezende Jardim');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Educação', 'Sônia Aparecida Barcelos Maciel');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Esportes, Lazer e Eventos', 'Roberto César de Oliveira Azevedo');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal da Fazenda', 'Marcio Rogério dos Santos');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Governo', 'Ricardo Prado Parreiras');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Meio Ambiente e Desenvolvimento Sustentavel', 'Daniel Hílario Lima Freitas');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Obras e Serviços Públicos', 'Alcimar Barcelos');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Planejamento e Coordenação', 'Vânia Alves Estevão');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Saúde', 'Junio de Araújo Alves');
-            INSERT INTO secretaries (name, responsible) VALUES ('Secretaria Municipal de Turismo e Cultura', 'Marcos Paulo de Andrade Amabis');
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Ação Social', 'Valcir Carlos Martins', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Administração', 'Flávio Capdeville de Meira', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Agricultura, Desenvolvimento Econômico, Pecuária e Abastecimento', 'Andressa Rezende Jardim', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Educação', 'Sônia Aparecida Barcelos Maciel', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Esportes, Lazer e Eventos', 'Roberto César de Oliveira Azevedo', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal da Fazenda', 'Marcio Rogério dos Santos', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Governo', 'Ricardo Prado Parreiras', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Meio Ambiente e Desenvolvimento Sustentavel', 'Daniel Hílario Lima Freitas', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Obras e Serviços Públicos', 'Alcimar Barcelos', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Planejamento e Coordenação', 'Vânia Alves Estevão', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Saúde', 'Junio de Araújo Alves', Carbon::now(), Carbon::now());
+            INSERT INTO secretaries (name, responsible, created_at, updated_at) VALUES ('Secretaria Municipal de Turismo e Cultura', 'Marcos Paulo de Andrade Amabis', Carbon::now(), Carbon::now());
         ");
     }
 }
